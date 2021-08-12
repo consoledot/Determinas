950b1d26f96749d22bb20563ea0cb91cb9e6f64c
consoledot <bimbooladele@outlook.com>
2021-08-12 01:12:07 +0100

    added location select

--- components/Talent Pool/index.jsx
+++ components/Talent Pool/index.jsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 export default function TalentPool() {
   return (
     <section className="talent-pool">
       <p>NEED TO PUT TOGETHER A TEAM OF TALENTS FOR YOUR NEXT PROJECT?</p>
-      <button>ACCESS OUR TALENT POOL</button>
+      <button>
+        <Link href="/talent-fellowship">ACCESS OUR TALENT POOL</Link>
+      </button>
     </section>
   );
 }
